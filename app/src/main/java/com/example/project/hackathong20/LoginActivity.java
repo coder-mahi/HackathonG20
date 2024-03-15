dbd4de17ec87aab2eb9ca52ef9f080474bdd79d6
mahesh <maheshshinde9100@gmail.com>
2024-03-15 14:40:12 +0530

    login and signup

--- app/src/main/java/com/example/project/hackathong20/LoginActivity.java
+++ app/src/main/java/com/example/project/hackathong20/LoginActivity.java
@@ -2,13 +2,25 @@ package com.example.project.hackathong20;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView signupbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        signupbtn = findViewById(R.id.signupbtn);
+        signupbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
