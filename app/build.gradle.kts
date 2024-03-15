28321d7129f428fceeb89394cc167e19fc6dfa03
mahesh <maheshshinde9100@gmail.com>
2024-03-15 14:19:14 +0530

    commit2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.project.hackathong20"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.project.hackathong20"
